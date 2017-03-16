131205ff4a62a55bdb34d90d1e75c396bf8110cb
Cristian Benitez <clbenitez64@gmail.com>
2017-03-16 10:49:21 -0500

    update hom spec

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 feature "Home Message", :type => :feature do
   context "User visits home page" do
-    it "should render Hola Mundo message" do
+    it "should render hola mundo message" do
       visit root_path
       expect(page).to have_content('hola mundo')
     end
